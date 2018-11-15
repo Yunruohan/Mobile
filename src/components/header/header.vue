d5fcafa38e3bf8b640ba112dfa1bde9c18e5e1e5
dmsun <dmsun@cpe-172-100-107-225.twcny.res.rr.com>
2018-11-15 15:08:37 +0800

    header页面尝试

---
+++ src/components/header/header.vue
@@ -0,0 +1,33 @@
+<template>
+    <header id='head_top'>
+         <!--<section class="head_goback">
+             <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" version="1.1">
+                 <polyline points="12,18 4,9 12,0" style="fill:none;stroke:rgb(255,255,255);stroke-width:2"/>
+             </svg>
+        </section>-->
+        <div>
+            
+        </div>
+    </header>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+<style lang="scss">
+    @import '../../style/mixin';
+
+    #head_top{
+        background-color: $blue;
+        position: fixed;
+        z-index: 100;
+        left: 0;
+        top: 0;
+        @include wh(100%, 1.95rem);
+    }
+</style>
